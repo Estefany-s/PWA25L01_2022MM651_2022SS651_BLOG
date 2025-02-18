b29c7c57d611eaafcf3c61094d13c46e0ed4af22
Estefany Sanabria <estefanysanabrias@gmail.com>
2025-02-18 17:41:24 -0600

    Configuraciones generales

---
+++ L01_2022MM651_2022SS651/Models/blogContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01_2022MM651_2022SS651.Models
+{
+    public class blogContext : DbContext
+    {
+        public blogContext(DbContextOptions<blogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Calificaciones> Calificaciones { get; set; }
+        public DbSet<Comentarios> Comentarios { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
--- L01_2022MM651_2022SS651/Program.cs
+++ L01_2022MM651_2022SS651/Program.cs
@@ -1,9 +1,19 @@
+using L01_2022MM651_2022SS651.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<blogContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("blogDbConnection")
+    )
+);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

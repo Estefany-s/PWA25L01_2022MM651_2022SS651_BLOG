adf78157a7671afeed340df578d27840af3490cb
Estefany Sanabria <estefanysanabrias@gmail.com>
2025-02-18 19:20:09 -0600

    CRUD de Calificaciones y comentarios, más los endpoints extra que se solicitaba.

--- L01_2022MM651_2022SS651/Models/Comentarios.cs
+++ L01_2022MM651_2022SS651/Models/Comentarios.cs
@@ -1,6 +1,14 @@
-﻿namespace L01_2022MM651_2022SS651.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace L01_2022MM651_2022SS651.Models
 {
     public class Comentarios
     {
+        [Key]
+        public int cometarioId { get; set; }
+        public int publicacionId { get; set; }
+        public string? comentario { get; set; }
+        public int usuarioId { get; set; }
     }
 }

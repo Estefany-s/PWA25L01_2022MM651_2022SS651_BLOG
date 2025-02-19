535d3e067047930404a7ff011a307fad2c1b934e
Angel Magana <maganaangel342@gmail.com>
2025-02-18 19:22:19 -0600

    CRUD de la usuario completado y agregación de modelo roles

--- L01_2022MM651_2022SS651/Models/Usuario.cs
+++ L01_2022MM651_2022SS651/Models/Usuario.cs
@@ -1,6 +1,18 @@
-﻿namespace L01_2022MM651_2022SS651.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace L01_2022MM651_2022SS651.Models
 {
+    [Table("usuarios")]
     public class Usuario
     {
+        [Key]
+        public int usuarioId {  get; set; }
+        public int rolId { get; set; }
+        public string nombreUsuario { get; set; }
+        public string clave {  get; set; }
+        public string nombre {  get; set; }
+        public string apellido { get; set; }
     }
 }

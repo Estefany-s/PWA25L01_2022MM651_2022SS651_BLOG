15f5d6b24a68a0579d60c366745e29fc8158533d
Angel Magana <maganaangel342@gmail.com>
2025-02-18 19:56:11 -0600

    Últimos cambios realizados correctamente

--- L01_2022MM651_2022SS651/Controllers/CalificacionesController.cs
+++ L01_2022MM651_2022SS651/Controllers/CalificacionesController.cs
@@ -1,5 +1,6 @@
 ﻿using L01_2022MM651_2022SS651.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2022MM651_2022SS651.Controllers
 {
@@ -45,6 +46,63 @@ namespace L01_2022MM651_2022SS651.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        //Actualizar Calificación
+        [HttpPut]
+        [Route("actualizarCalification/{id}")]
+        public IActionResult actualizarCalificacion(int id, [FromBody] Calificaciones actualizarCalificacion)
+        {
+            Calificaciones? calificacionActual = (from c in _blogContexto.Calificaciones where c.calificacionId == id select c).FirstOrDefault();
+            if (calificacionActual == null)
+            {
+                return NotFound();
+            }
+
+            calificacionActual.calificacion = actualizarCalificacion.calificacion;
+
+            _blogContexto.Entry(calificacionActual).State = EntityState.Modified;
+            _blogContexto.SaveChanges();
+
+            return Ok(actualizarCalificacion);
+        }
+
+        //Eliminar calificación
+        [HttpDelete]
+        [Route("eliminarCalification/{id}")]
+        public IActionResult EliminarCalificacion(int id)
+        {
+            Calificaciones? calificacion = (from c in _blogContexto.Calificaciones where c.calificacionId == id select c).FirstOrDefault();
+
+            if (calificacion == null) { return NotFound(); }
+
+            _blogContexto.Calificaciones.Attach(calificacion);
+            _blogContexto.Calificaciones.Remove(calificacion);
+            _blogContexto.SaveChanges();
+
+            return Ok(calificacion);
+        }
+
+        //Filtrar listado de calificaciones por publicacion
+        [HttpGet]
+        [Route("obtenerCalificacionXPubli/{id}")]
+        public IActionResult obtenerCalificacionXPublicacion(int id)
+        {
+            var calificacionXPubli = (from c in _blogContexto.Calificaciones
+                                 join p in _blogContexto.Publicaciones
+                                    on c.publicacionId equals p.publicacionId
+                                 where p.publicacionId == id
+                                 select new
+                                 {
+                                     p.titulo,
+                                     c.calificacion
+                                 }).ToList();
+
+            if (calificacionXPubli == null)
+            {
+                return NotFound();
+            }
+            return Ok(calificacionXPubli);
         }
     }
 }
---
+++ L01_2022MM651_2022SS651/Models/Publicaciones.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2022MM651_2022SS651.Models
+{
+    public class Publicaciones
+    {
+        [Key]
+        public int publicacionId {  get; set; }
+        public string titulo { get; set; }
+        public string descripcion {  get; set; }
+        public int usuarioId { get; set; }
+    }
+}
